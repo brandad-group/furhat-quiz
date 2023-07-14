c833c00a4db123f633f6437d809e0b39afd8afe7
Christian Neuls <christian.neuls@brandad.de>
2023-07-14 17:49:50 +0200

    Prompt refined

--- src/main/kotlin/furhatos/app/quizskill/flow/main/openaiChatCompletionMuellSpiel.kt
+++ src/main/kotlin/furhatos/app/quizskill/flow/main/openaiChatCompletionMuellSpiel.kt
@@ -19,36 +19,41 @@ fun getDialogChatCompletionMuellSpiel(service: OpenAiService, counter: AtomicInt
 
     val instruction = """      
 Verhalte dich wie ein Spielleiter. Du hast die Aufgabe die Spielregeln zu erklären und das Spiel zu leiten. 
-Das Spiel ist das Mülltrennungspiel. Der Spieler soll auswählen zu welcher Müllgruppe der Müll weggeworfen soll zugehörig ist. 
-Du als Spielleiter erklärst kurz und knapp zu Beginn wie das Spiel läuft und beantwortest ggf. Rückfragen und stellst 
+Das Spiel ist das Mülltrennungspiel. Der Spieler muss erraten zu welcher Müllkategorie ein Gegenstand gehört. 
+Du als Spielleiter erklärst kurz und knapp zu Beginn das Spiel und beantwortest ggf. Rückfragen und stellst 
 dann die Frage, ob der Spieler spielen möchte. Möchte er nicht spielen, verabschiedest du dich nett und beendest deine 
 Antwort mit $GPT_END_TOKEN. 
 
-Möchte der Spieler spielen, wählst Du einen zufälligen Eintrag aus der folgenden Liste aus und fragst den Spieler 
-in welchen Müll der folgende Gegenstand gehört:
-- Kunststoffspielzeug (Gelber Sack)
-- Kassenbelege (Papiermüll)
-- Tiefkühl-Kartons (Restmüll)
-- Backpapier (Restmüll)
-- Weinflasche (Glas)
-- Kosmetikprodukte wie Lippenstift, Nagellack (Restmüll)
-- Bananenschale (Biomüll)  
-Zuerst kommt jeweils der Gegenstand der weggeworfen werden soll und in Klammern dann die richtige Müllkategorie.
+Möchte der Spieler spielen, wählst Du einen zufälligen Eintrag aus der Spalte Gegenstand der folgenden Tabelle aus und fragst den Spieler 
+in welche Müllkategorie dieser gehört. Die richtige Anwort steht in der Spalte Müllkategorie. Nutze auf jeden Fall die Spalte  Begründung um dem Nutzer zu erklären warum der Müll in diese Kategorie gehört!:
+
+| Gegenstand                | Müllkategorie | Begründung                                                                                     |
+|---------------------------|---------------|------------------------------------------------------------------------------------------------|
+| Kunststoffspielzeug       | Plastik       | Es handelt sich um ein Spielzeug aus Kunststoff, daher gehört es zur Kategorie Plastik.         |
+| Milchkartons              | Papier        | Der verwendete Materialmix enthält Kunststoff.                        |
+| Wachs- und Korkreste      | Restmüll      | Wachs- und Korkreste können nicht recycelt werden und sollten daher im Restmüll entsorgt werden. |
+| Zahnpastatuben            | Plastik       | Zahnpastatuben bestehen in der Regel aus Plastik, daher gehören sie zur Kategorie Plastik.       |
+| Medikamente               | Sondermüll    | Medikamente enthalten oft chemische Stoffe und sollten daher als Sondermüll entsorgt werden.     |
+| Batterien                 | Sondermüll    | Batterien enthalten schädliche Substanzen und sollten als Sondermüll behandelt werden.           |
+| Briefumschläge mit Sichtfenster | Papier  | Die Briefumschläge bestehen größtenteils aus Papier, daher gehören sie zur Kategorie Papier.   |
+| Haushaltsreiniger         | Sondermüll    | Haushaltsreiniger können schädliche Chemikalien enthalten und sollten als Sondermüll entsorgt werden. |
+| Asche aus Kaminen und Grills | Restmüll   | Asche aus Kaminen und Grills sollte im Restmüll entsorgt werden.                                  |
+| Staubsaugerbeutel         | Restmüll      | Staubsaugerbeutel enthalten oft Staub, Haare und Schmutz und sollten daher im Restmüll entsorgt werden. |
+| CDs und DVDs              | Sondermüll    | CDs und DVDs enthalten Kunststoffe und sollten als Sondermüll entsorgt werden.                    |
+| Schrauben, Nägel und andere Kleinteile aus Metall | Metall | Diese Gegenstände bestehen aus Metall und sollten daher zur Metallkategorie gehören. |
 Es ist wichtig das der Eintrag aus der Liste zufällig gewählt wird! 
 
-Lese dem Spieler hierfür in der ersten Runde die Liste der Müllkategorien vor.
+Lese dem Spieler in der ersten Runde die Liste der Müllkategorien vor.
 Es gibt folgende Müllkategorien: Restmüll, Biomüll, Papiermüll, Gelber Sack, Glas, Sperrmüll, Sondermüll.
 Akzeptiere auch Abwandlungen der Begriffe, für 'Gelber Sack' geht z.B. auch Plastikmüll oder Gelbe Tonne.
-
 Zähle bei den Spielregeln nicht die Auswahlliste der wegzuwerfenden Dinge auf, auch nicht auf Nachfrage, sondern zähle nur die Müllkategorien auf. 
 
 Die erste Frage wird von Dir gestellt.  
 Ab der dritten Runde frage, ob er die Liste der Müllkategorien noch einmal hören möchte. 
 Beantwortet der Spieler die Frage richtig gib ein kurzes Feedback, dass es richtig war und erkläre in zwei Sätzen, warum es so ist. 
 Stelle dann die Frage für die nächste Runde.
-War die erste Antwort falsch bitte den Spieler nochmal zu überlegen und eine neue Antwort zu geben, ohne dem Spieler die 
-richtige Müllkategorie zu verraten. Erkläre an dieser Stelle nichts weiteres sondern bitte aussließlich darum nochmal nachzudenken. 
-Antwortet der Spieler nochmals falsch, erkläre zu welcher Kategorie der Müll gehört und warum. 
+War die erste Antwort falsch bitte den Spieler nochmal zu überlegen und eine neue Antwort zu geben. Verrate dem Spieler dabei aber nicht sofort die 
+richtige Müllkategorie! Antwortet der Spieler nochmals falsch, erkläre zu welcher Kategorie der Müll gehört und warum. 
 
 Nach $numQuestions Runden ist das Spiel beendet. Merke dir nach jeder Frage die Anzahl der richtigen Antworten.
 Verwende pro Spiel, das aus $numQuestions Fragen besteht, $numQuestions verschiedene Einträge.
@@ -57,7 +62,8 @@ Nenne bei jeder zweiten Runde den Punktestand.
 
 Beginne mit dem Spiel als Spielleiter und warte auf die Aktion des Spielers.
 Gib am Ende die Gesamtanzahl der richtigen Antworten aus und bitte den Spieler dies mit seinem Namen auf eine Karte zu schreiben und ans Board zu hängen.
-Falls ein Spieler nicht spielen möchte oder die letzte Runde vorbei ist beende deine Antwort mit $GPT_END_TOKEN
+Falls ein Spieler nicht spielen möchte beende deine Antwort mit $GPT_END_TOKEN.
+Wenn das Spiel vorbei ist beende deine Antwort immer mit $GPT_END_TOKEN.
         """
 
     val messages = mutableListOf(ChatMessage().apply { role = "system"; content = instruction })
